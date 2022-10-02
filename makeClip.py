6323505716e0208dcf339b863511eb137dbda3ac
Fabian Winkler <winkler.fabian22@gmx.at>
2022-10-02 20:45:59 +0200

    added find scene and get starttime & endtime from clip
    - need to fix if statement from timestamps & cuts

--- makeClip.py
+++ makeClip.py
@@ -8,6 +8,7 @@ import moviepy.editor as mp
 import re
 from moviepy.editor import VideoFileClip
 import time
+from scenedetect import open_video, ContentDetector, SceneManager, StatsManager
 
 
 def cut_video(video_title, start_time, duration):
@@ -92,6 +93,105 @@ def add_text_to_video(comment):
         # txt_mov = txt_col.set_pos( lambda t: (max(w/30,int(w-0.5*w*t)),max(5*h/6,int(100*t))) )
         final = mp.CompositeVideoClip([my_video, txt_col.set_position((0, my_video.h - my_text.h - 90)),txt_col2.set_position((0, my_video.h - my_text.h - 50))])
         final.subclip(0, 20).write_videofile("test.mp4", fps=30, codec="libx264")
+
+def get_startTime_and_endTime(url):
+    cluster = pymongo.MongoClient(
+        "mongodb+srv://admin:JyggcLIE4DGsQtu8@cluster0.0lwnskf.mongodb.net/?retryWrites=true&w=majority")
+    db = cluster["test"]
+    collection = db["timestamps"]
+    pymongo_cursor = collection.find()
+    all_data = list(pymongo_cursor)
+
+
+    comment_arr = []
+    author_arr = []
+    URL_arr = []
+    timestamp_arr = []
+
+    for i in range(len(all_data)):
+        comment = all_data[i]["Comment"]
+        comment = re.sub('\d+\d+:+\d+\d', '', comment)
+        comment = re.sub('\d+:+\d+\d', '', comment)
+        comment_arr.append(comment)
+        author_arr.append(all_data[i]["Author"])
+        URL_arr.append(all_data[i]["URL"])
+
+        if len(all_data[i]["Timestamp"]) == 4:  # add a 0 if start_time string 0:00 format 00:00 required
+            to_add = "0"
+            res = "".join((to_add, all_data[i]["Timestamp"]))
+        else:
+            res = all_data[i]["Timestamp"]
+
+        timestamp_arr.append(res)
+
+
+
+    comment_author_url_timestamp_df = pd.DataFrame({"Comment": comment_arr, "Author": author_arr, "URL": URL_arr, "Timestamp": timestamp_arr})
+    filtered_df = comment_author_url_timestamp_df.loc[comment_author_url_timestamp_df["URL"] == url]
+
+    timestamps = []
+
+    for i in range(len(comment_author_url_timestamp_df)):
+        timestamp = comment_author_url_timestamp_df.loc[i]["Timestamp"]
+        as_secounds = int(timestamp[0:2]) * 60 + int(timestamp[3:5])
+        timestamps.append(as_secounds)
+
+
+
+    begin_of_scenes = find_scenes("video1.mp4")
+    start_and_endtime = {"Starttime":[], "Endtime":[]}
+
+    for i in range(len(timestamps)):
+        for y in range(len(begin_of_scenes)):
+            if timestamps[i] <= begin_of_scenes[y]+10 and timestamps[i] <= begin_of_scenes[y]+15:
+                if timestamps[i]-begin_of_scenes[y] <= 10:
+                    start_and_endtime["Starttime"].append(timestamps[i])
+                    start_and_endtime["Endtime"].append(begin_of_scenes[y])
+                start_and_endtime["Starttime"].append(timestamps[i])
+                start_and_endtime["Endtime"].append(begin_of_scenes[y])
+                break
+            elif y ==len(begin_of_scenes):
+                start_and_endtime["Starttime"].append(timestamps[i])
+                start_and_endtime["Endtime"].append(timestamps[i]+15)
+
+
+
+    print("")
+
+
+def find_scenes(video_path):
+    # type: (str) -> List[Tuple[FrameTimecode, FrameTimecode]]
+
+    video_stream = open_video(video_path)
+    stats_manager = StatsManager()
+    # Construct our SceneManager and pass it our StatsManager.
+    scene_manager = SceneManager(stats_manager)
+
+    # Add ContentDetector algorithm (each detector's constructor
+    # takes various options, e.g. threshold).
+    scene_manager.add_detector(ContentDetector(threshold=55.0))
+
+    # Save calculated metrics for each frame to {VIDEO_PATH}.stats.csv.
+    stats_file_path = '%s.stats.csv' % video_path
+
+    # Perform scene detection.
+    scene_manager.detect_scenes(video=video_stream, show_progress=True)
+    scene_list = scene_manager.get_scene_list()
+    # for i, scene in enumerate(scene_list):
+    #     print(
+    #         'Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
+    #         i+1,
+    #         scene[0].get_timecode(), scene[0].get_frames(),
+    #         scene[1].get_timecode(), scene[1].get_frames(),))
+
+    begin_of_scenes = []
+    for i in range(len(scene_list)):
+        as_timecode = str(scene_list[i][0])[3:8]
+        as_secounds = int(as_timecode[0:2]) * 60 + int(as_timecode[3:5])
+
+        begin_of_scenes.append(as_secounds)
+
+    return begin_of_scenes
 
 def main():
     pass
